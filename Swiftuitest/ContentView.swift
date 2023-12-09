f96724b6ab353d2aa3b570284ffe5e3bd35d4d66
sannma31 <kasaisyouun2004@gmail.com>
2023-12-10 00:41:34 +0900

    Initial Commit

---
+++ Swiftuitest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Swiftuitest
+//
+//  Created by 笠井翔雲 on 2023/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swiftuitest/SwiftuitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuitestApp.swift
+//  Swiftuitest
+//
+//  Created by 笠井翔雲 on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
